24ae8f6f55e6232231504222e62569ec76cf1d78
Luis Alberto Flores Garrido <lalbertofloga@gmail.com>
2023-11-19 08:06:12 +0100

    Acttu 19/11/23

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
+
 public class Ejercicio5 {
-    /* Enunciado: Escribe un programa que lea dos números, calcule y muestre el valor de su suma, resta, multiplicación y su división. */
+    /* Enunciado: Escribe un programa que calcule la longitud, área y volumen de un ciruculo del cual la información del radio que entra por teclado. */
     public static void main(String [] args){
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Escriba el primer número con el que quiera operar");
-        int numero1 = entrada.nextInt();
-        System.out.println("Escriba el segundo número con el que quiera operar");
-        int numero2 = entrada.nextInt();
-        int resultadosuma = numero1+numero2;
-        int resultadoresta = numero1-numero2;
-        int resultadomult = numero1*numero2;
-        int resultadodivi = numero1/numero2;
-        
-        System.out.println("La suma de " +numero1 +" y de " +numero2 + " tiene como resultado: " +resultadosuma);
-              System.out.println("La resta de " +numero1 +" y de " +numero2 + " tiene como resultado: " +resultadoresta);
-                    System.out.println("La multiplicación de " +numero1 +" por " +numero2 + " tiene como resultado: " +resultadomult);
-                          System.out.println("La división de " +numero1 +" entre  " +numero2 + " tiene como resultado: " +resultadodivi);
-        
+        System.out.println("Escriba la medida del radio de su círculo");
+        float radio = entrada.nextInt();
+        final double pi = Math.PI;
+        double circunfe = 2*pi*radio;
+        double area = pi*Math.pow	(radio,2);
+        double volumen = 4/3 * pi*Math.pow (radio,3);
+        System.out.println("La medida de su circunferencia es de: " +circunfe);
+        System.out.println("La medida de area es de: " +area);
+        System.out.println("La medida de su volumen es de: " +volumen);
         entrada.close();
     }
 }
